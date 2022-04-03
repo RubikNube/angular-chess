3eb2d4f48408ae991e43425c4ba67d90117a4d9b
RubikNube <roland@kiesendahl.de>
2022-04-03 22:11:18 +0200

    first code for scroll down fix

--- angular-chess/src/app/components/move-history/move-history.component.ts
+++ angular-chess/src/app/components/move-history/move-history.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChild } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { ChessBoardService } from 'src/app/services/chess-board.service';
 import { MoveHistoryService } from 'src/app/services/move-history.service';
 import { Board } from 'src/app/types/board.t';
@@ -14,24 +15,35 @@ import PositionUtils from 'src/app/utils/position.utils';
   styleUrls: ['./move-history.component.scss']
 })
 export class MoveHistoryComponent {
+  @ViewChild('move')
+  historyTable!: QueryList<ElementRef>;
+
   fullMoveHistory: FullMove[] = [];
-  public selectedMove: FullMove = { count: 0 };
   public menuItems: MenuItem[] = [
-    { label: "white", command: () => this.resetBoard(this.selectedMove.whiteMove?.board) },
-    { label: "black", command: () => this.resetBoard(this.selectedMove.blackMove?.board) }
+    { label: "white", command: () => this.resetBoard(this.selectedMove$$.getValue().whiteMove?.board) },
+    { label: "black", command: () => this.resetBoard(this.selectedMove$$.getValue().blackMove?.board) }
   ];
+  public selectedMove$$: BehaviorSubject<FullMove> = new BehaviorSubject<FullMove>({ count: 0 });
+  public selectedMove$: Observable<FullMove> = this.selectedMove$$.asObservable();
 
   constructor(private moveHistoryService: MoveHistoryService,
     public boardService: ChessBoardService) {
     this.moveHistoryService.getFullMoveHistory$().subscribe(
       p => {
         this.fullMoveHistory = p;
+        this.selectedMove$$.next(p[p.length - 1]);
+        let table = this.historyTable;
+        this.historyTable.toArray()[(p.length - 1)].nativeElement.focus();
       }
     );
+
+    this.selectedMove$.subscribe(p => {
+      console.log("### selectedMove$: " + JSON.stringify(p));
+    })
   }
 
   public resetBoard(board: Board | undefined): void {
-    console.log("selectedMove: " + JSON.stringify(this.selectedMove));
+    console.log("selectedMove: " + JSON.stringify(this.selectedMove$$.getValue()));
     console.log("resetBoard: " + JSON.stringify(board));
 
     if (board !== undefined) {
