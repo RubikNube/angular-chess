1bf76eb5fe89df7c761e81315ed83066699a35ee
cheg9737 <christian.eggert@elektrobit.com>
2022-06-21 15:56:04 +0200

    test

--- angular-chess/src/app/app.component.ts
+++ angular-chess/src/app/app.component.ts
@@ -1,9 +1,13 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { environment } from './../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [{ provide: APP_BASE_HREF, useValue: environment.production ? './' : '/' }]
 })
 export class AppComponent {
 }
