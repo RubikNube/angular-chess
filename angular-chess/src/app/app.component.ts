e10b73f12102510d426d4798eee49113bf390c46
RubikNube <roland@kiesendahl.de>
2022-07-04 15:26:59 +0200

    revert changes

--- angular-chess/src/app/app.component.ts
+++ angular-chess/src/app/app.component.ts
@@ -6,8 +6,7 @@ import { environment } from './../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  providers: [{ provide: APP_BASE_HREF, useValue: environment.production ? './' : '/' }]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 }
