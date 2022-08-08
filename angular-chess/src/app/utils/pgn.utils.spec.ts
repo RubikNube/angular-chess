f9fddc1a3e1b6a70cf52aed5dd885028a0f30b2e
RubikNube <roland@kiesendahl.de>
2022-08-08 23:16:13 +0200

    created regex for moveGroups

--- angular-chess/src/app/utils/pgn.utils.spec.ts
+++ angular-chess/src/app/utils/pgn.utils.spec.ts
@@ -3,7 +3,19 @@ import { Move, PieceType } from "../types/pieces.t";
 import PgnUtils from "./pgn.utils";
 
 describe('PgnUtils', () => {
-  const pgnToImport: string = `[Event "IBM Kasparov vs. Deep Blue Rematch"]
+  const pgnToImport: string = `[Event "IBM Kasparov vs. Deep Blue Rematch"]\n
+[Site "New York, NY USA"]\n
+[Date "1997.05.11"]\n
+[Round "6"]\n
+[White "Deep Blue"]\n
+[Black "Kasparov, Garry"]\n
+[Opening "Caro-Kann: 4...Nd7"]\n
+[ECO "B17"]\n
+[Result "1-0"]\n
+ 
+1.e4 c6 2.d4 d5 3.Nc3\n`;
+
+  const wholeGameImport: string = `[Event "IBM Kasparov vs. Deep Blue Rematch"]
 [Site "New York, NY USA"]
 [Date "1997.05.11"]
 [Round "6"]
@@ -13,7 +25,10 @@ describe('PgnUtils', () => {
 [ECO "B17"]
 [Result "1-0"]
  
-1.e4 c6 2.d4 d5`;
+1.e4 c6 2.d4 d5 3.Nc3 dxe4 4.Nxe4 Nd7 5.Ng5 Ngf6 6.Bd3 e6 7.N1f3 h6
+8.Nxe6 Qe7 9.O-O fxe6 10.Bg6+ Kd8 {Kasparov schüttelt kurz den Kopf}
+11.Bf4 b5 12.a4 Bb7 13.Re1 Nd5 14.Bg3 Kc8 15.axb5 cxb5 16.Qd3 Bc6
+17.Bf5 exf5 18.Rxe7 Bxe7 19.c4 1-0`;
 
   const positionD2 = { column: 4, row: 2 };
   const positionD4 = { column: 4, row: 4 };
@@ -34,7 +49,7 @@ describe('PgnUtils', () => {
 
   describe('extractMovesFromPgn', () => {
     it('should be able to extract pawn moves', () => {
-      expect(PgnUtils.extractMovesFromPgn(pgnToImport)).toEqual(expextedMoves);
+      expect(PgnUtils.extractMovesFromPgn(wholeGameImport)).toEqual(expextedMoves);
     });
   });
 });
--- angular-chess/src/app/utils/pgn.utils.ts
+++ angular-chess/src/app/utils/pgn.utils.ts
@@ -2,10 +2,33 @@ import { Move } from "../types/pieces.t";
 
 export default class PgnUtils {
   public static extractMovesFromPgn(newPgn: string): Move[] {
-    var moves: Move[] = [];
+    const moves: Move[] = [];
 
-    var beginOfMovesIndex = newPgn.search('^1\.');
-    var moveString = newPgn.substring(beginOfMovesIndex);
+    const beginOfMovesIndex = newPgn.search(/\n\d../);
+    const rawMoveString = newPgn.substring(beginOfMovesIndex);
+
+    console.error(`index: ${beginOfMovesIndex} move string: ${rawMoveString} lenght: ${rawMoveString.length}`)
+
+    const kingSideCastleRegEx = /O-O/;
+    const queenSideCastleRegEx = /O-O-O/;
+    const coordinateRegEx = /[a-h][1-8]/;
+    const pieceCharRegEx = new RegExp(`(K|Q|R|B|N)([a-h]|[1-8])?(${coordinateRegEx.source})?`);
+    const pieceRegEx = new RegExp(`${pieceCharRegEx.source}?${coordinateRegEx.source}`);
+    const moveRegEx = new RegExp(`(${coordinateRegEx.source}|${pieceRegEx.source}|${kingSideCastleRegEx.source}|${queenSideCastleRegEx.source})`);
+    const captureRegEx = new RegExp(`(${pieceCharRegEx.source}|[a-h])x${coordinateRegEx.source}`);
+    const moveOrCaptureRegEx = new RegExp(`(${moveRegEx.source}|${captureRegEx.source})\\+?`);
+    const moveGroupRegEx = `(\\d+\.${moveOrCaptureRegEx.source} ${moveOrCaptureRegEx.source}|\\d+\.${moveOrCaptureRegEx.source})`;
+    console.error(`moveGroupRegEx: ${moveGroupRegEx}`)
+    const moveStrings = [...rawMoveString.matchAll(new RegExp(moveGroupRegEx, 'gm'))];
+    console.error(`moveStrings: ${moveStrings} moveStrings.lenght: ${moveStrings.length}`)
+
+
+    for (const moveString in moveStrings) {
+      console.error(`moveString: ${moveString}`)
+    }
+
+
+
 
     return moves;
   }
