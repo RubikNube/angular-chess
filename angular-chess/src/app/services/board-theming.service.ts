f0760fc492c1b669f0857437a51d1d9c796e0935
RubikNube <roland@kiesendahl.de>
2023-10-15 22:54:09 +0200

    Added view menu entry for dark mode

--- angular-chess/src/app/services/board-theming.service.ts
+++ angular-chess/src/app/services/board-theming.service.ts
@@ -11,7 +11,6 @@ export interface BoardThemeConfig {
   providedIn: 'root'
 })
 export class BoardThemingService {
-
   private readonly woodTheme: BoardThemeConfig = {
     darkField: 'url(./assets/board_field_dark.png)',
     lightField: 'url(./assets/board_field_light.png)'
@@ -22,20 +21,44 @@ export class BoardThemingService {
     lightField: '#c19a6b',
   };
 
+  private readonly darkModeTheme: BoardThemeConfig = {
+    darkField: '#252850',
+    lightField: '#87ceff',
+  };
+
   private readonly selectedTheme$$: BehaviorSubject<BoardThemeConfig> = new BehaviorSubject(this.woodTheme);
   public readonly selectedTheme$: Observable<BoardThemeConfig> = this.selectedTheme$$.asObservable();
+
+  // add a observable for the dark mode
+  private readonly isDarkModeActive$$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  public readonly isDarkModeActive$: Observable<boolean> = this.isDarkModeActive$$.asObservable();
 
   private constructor(private persistenceService: PersistenceService) {
     const persistedTheme = this.persistenceService.load('selectedTheme');
     if (persistedTheme) {
       this.selectedTheme$$.next(persistedTheme);
     }
-   }
+  }
 
   public toggleTheme(): void {
     const actualTheme = this.selectedTheme$$.getValue();
     const newTheme = actualTheme === this.woodTheme ? this.brownTheme : this.woodTheme;
     this.persistenceService.save('selectedTheme', newTheme);
     this.selectedTheme$$.next(newTheme);
+  }
+
+  public toggleDarkMode(): void {
+    if (this.isDarkModeActive$$.getValue()) {
+      this.isDarkModeActive$$.next(false);
+      // set board theme to brown
+      this.persistenceService.save('selectedTheme', this.brownTheme);
+      this.selectedTheme$$.next(this.brownTheme);
+    }
+    else {
+      this.isDarkModeActive$$.next(true);
+      // set board theme to dark mode
+      this.persistenceService.save('selectedTheme', this.darkModeTheme);
+      this.selectedTheme$$.next(this.darkModeTheme);
+    }
   }
 }
