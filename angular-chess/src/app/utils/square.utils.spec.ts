36125e0f584d49b9235c654c88716c6679a842d0
RubikNube <roland@kiesendahl.de>
2024-04-29 23:21:24 +0200

    Add unit tests for SquareUtils.convertSquareToPosition in square.utils.spec.ts

--- angular-chess/src/app/utils/square.utils.spec.ts
+++ angular-chess/src/app/utils/square.utils.spec.ts
@@ -58,4 +58,60 @@ describe('SquareUtils', () => {
       expect(actualSquare).toEqual(expectedSquare);
     });
   });
+
+  describe('convertPositionToSquare', () => {
+    it('should convert square a1 to position (r: 1, c: 1)', () => {
+      const square: Square = Square.SQ_A1;
+      const expectedPosition: Position = { row: 1, column: 1 };
+
+      const actualPosition: Position = SquareUtils.convertSquareToPosition(square);
+
+      expect(actualPosition).toEqual(expectedPosition);
+    });
+
+    it('should convert square a2 to position (r: 1, c: 2)', () => {
+      const square: Square = Square.SQ_A2;
+      const expectedPosition: Position = { row: 1, column: 2 };
+
+      const actualPosition: Position = SquareUtils.convertSquareToPosition(square);
+
+      expect(actualPosition).toEqual(expectedPosition);
+    });
+
+    it('should convert square e2 to position (r: 5, c: 2)', () => {
+      const square: Square = Square.SQ_E2;
+      const expectedPosition: Position = { row: 5, column: 2 };
+
+      const actualPosition: Position = SquareUtils.convertSquareToPosition(square);
+
+      expect(actualPosition).toEqual(expectedPosition);
+    });
+
+    it('should convert square e3 to position (r: 5, c: 3)', () => {
+      const square: Square = Square.SQ_E3;
+      const expectedPosition: Position = { row: 5, column: 3 };
+
+      const actualPosition: Position = SquareUtils.convertSquareToPosition(square);
+
+      expect(actualPosition).toEqual(expectedPosition);
+    });
+
+    it('should convert square h1 to position (r: 8, c: 1)', () => {
+      const square: Square = Square.SQ_H1;
+      const expectedPosition: Position = { row: 8, column: 1 };
+
+      const actualPosition: Position = SquareUtils.convertSquareToPosition(square);
+
+      expect(actualPosition).toEqual(expectedPosition);
+    });
+
+    it('should convert square h8 to position (r: 8, c: 8)', () => {
+      const square: Square = Square.SQ_H8;
+      const expectedPosition: Position = { row: 8, column: 8 };
+
+      const actualPosition: Position = SquareUtils.convertSquareToPosition(square);
+
+      expect(actualPosition).toEqual(expectedPosition);
+    });
+  });
 });
--- angular-chess/src/app/utils/square.utils.ts
+++ angular-chess/src/app/utils/square.utils.ts
@@ -5,4 +5,11 @@ export default class SquareUtils {
   public static convertPositionToSquare(position: Position): Square {
     return (position.column - 1) * 8 + position.row - 1;
   }
+
+  public static convertSquareToPosition(square: Square): Position {
+    return {
+      row: (square % 8) + 1,
+      column: Math.floor(square / 8) + 1
+    };
+  }
 }
